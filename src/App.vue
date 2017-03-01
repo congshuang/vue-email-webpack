c40cd22c634408e97de9350ef976c2f6f589fbba
congshuang <1461373703@qq.com>
2017-03-01 15:21:14 +0800

    加了一个kissy 这里加了jquery和loading、router、axios

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-
+     <h1>woshi1nidaye1</h1>
   </div>
 </template>
 
 <script>
+//引入状态
+import {mapGetters, matpActions} from 'vuex'
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },computed:mapGetters([
+        'loading'
+     ]),
+    components:{
+
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,24 +3,35 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './router.config.js'
 import axios from 'axios'
+import store from './store/index.js'
 import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 import filters from './filters'
+import Loading from './components/loading'
+import Jquery from './assets/js/jquery-1.7.2.js'
+import 'kissy/build/editor/theme/cool/editor-min.css'
+import './assets/css/kissy_base.css'
+
 
 //Vue.filter(名字,函数)
 //循环遍历所有过滤器
 Object.keys(filters).forEach((key) => Vue.filter(key, filters[key]));
 
 Vue.use(VueRouter);
+Vue.use(Loading);
+Vue.use(Jquery);
 Vue.use(ElementUi);
 
 //关于axios配置
 axios.interceptors.request.use(function(config) { //发送请求
+    store.dispatch('showLoading')
     return config;
 }, function(error) {
     return Promise.reject(error);
 });
 
 axios.interceptors.response.use(function(response) { //请求回来
+    store.dispatch('hideLoading')
     return response;
 }, function(error) {
     return Promise.reject(error);
